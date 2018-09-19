7dbaeb7be63eaad13e9707dcf451f62ae629c80f
TWalk007 <walkertb03@gmail.com>
2018-09-19 12:07:31 -0400

    UI Context Menu System in and Working

---
+++ Assets/Scripts/ContextMenuSystem.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContextMenuSystem : MonoBehaviour {
+
+    [Header("Buttons")]
+    public Button backButton;
+    public Button nextButton;
+    public Button plusButton;
+    public Button minusButton;
+
+
+
+    private SteamVR_TrackedObject trackedObj;
+
+    private GameObject canvasObj;
+    private ControllerGrabObject controllerGrabObject;
+    private SteamVR_Controller.Device Controller {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+
+
+    private void Awake() {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    void Start() {
+        controllerGrabObject = GameObject.FindGameObjectWithTag("Controllers").GetComponent<ControllerGrabObject>();
+        canvasObj = transform.GetChild(0).gameObject;
+    }
+
+    public void ExitButton() {
+        controllerGrabObject.myState = ControllerGrabObject.States.freeRoam;
+        controllerGrabObject.menuOpen = false;
+        Destroy(gameObject);
+    }
+
+
+    //This will "grey out" the minus or plus button allowing us to use it or not based upon where it is in the hierarchy.
+    //private void SelectionHierarchyCheck() {
+    //    if (objectInHand.tag == "PasteHorizontal" || objectInHand.tag == "PasteVertical" || objectInHand.tag == "RinseBottle") {
+    //        if (objectInHand.transform.parent.tag != "PasteHorizontal" || objectInHand.transform.parent.tag != "PasteVertical" || objectInHand.transform.parent.tag != "RinseBottle") {
+    //            plusButton.interactable = false;
+    //        } else {
+    //            minusButton.interactable = false;
+    //        }
+    //    }
+    //}
+
+    //When this button is selected, it will make the parent of the object in hand active.  This will show the group of objects switch to the highlighted material all at once.
+    //public void ExpandSelection() {
+    //    foreach (Transform child in transform.parent) {
+    //        Material[] materials = child.GetComponent<HighlightController>().materials;
+    //        child.GetComponent<Renderer>().material = materials[1];
+    //    }
+    //}
+
+    //public void ShrinkSelection() {
+    //    foreach (Transform child in transform.parent) {
+    //        Material[] materials = child.GetComponent<HighlightController>().materials;
+    //        child.GetComponent<Renderer>().material = materials[0];
+    //    }
+    //    objectInHand.GetComponent<Renderer>().material = objectInHand.GetComponent<HighlightController>().materials[1];
+    //}
+
+
+}
--- Assets/Scripts/ControllerGrabObject.cs
+++ Assets/Scripts/ControllerGrabObject.cs
@@ -1,144 +1,198 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControllerGrabObject : MonoBehaviour {
 
-	public float rotationSpeed = 50.0f;
+    public GameObject parabolicPointer;
 
-	private float currentRot;
-	private float newRot;
-	private Vector3 angles;
+    [HideInInspector]
+    public GameObject objectInHand;
+    [HideInInspector]
+    public GameObject collidingObject;
+    [HideInInspector]
+    public bool menuOpen = false;
 
-	private SteamVR_TrackedObject trackedObj;
-	private GameObject collidingObject;
-	private GameObject objectInHand;
-
-	private bool objectInHandCheck = false;
-
-	private SteamVR_Controller.Device Controller
-	{
-		get { return SteamVR_Controller.Input((int)trackedObj.index); }
-	}
-
-	private void Awake()
-	{
-		trackedObj = GetComponent<SteamVR_TrackedObject>();
-	}
-
-	private void SetCollidingObject(Collider col)
-	{
-		if (collidingObject || !col.GetComponent<Rigidbody>())
-		{
-			return;
-		}
-		collidingObject = col.gameObject;
-	}
-
-	public void OnTriggerEnter(Collider other)
-	{
-		SetCollidingObject(other);
+    public GameObject contextMenuSystem;
+    public enum States { freeRoam, objectHighlighted, objectInHand, menuActive };
+    public States myState;
+       
 
 
-		//TODO  This does not take into account the current rotation value of the object when starting the rotation.
-		//      For example, if it's at 200° it might appear backwards to you but you might want to push it the other way instead
-		//      and get the reverse result of your intention.
-		if (Controller.GetAxis() != Vector2.zero)
-		{
-			if (Controller.GetAxis().y < 0f)
-			{
-				other.transform.Rotate(Vector3.up, rotationSpeed * Controller.GetAxis().y * 10 * Time.deltaTime);
-			}
-			if (Controller.GetAxis().y > 0f)
-			{
-				other.transform.Rotate(Vector3.down, rotationSpeed * -Controller.GetAxis().y * 10 * 2 * Time.deltaTime);
-			}
-		}
-	}
+    private SteamVR_TrackedObject trackedObj;
+    private bool objectInHandCheck = false;
+    private bool parabolicPointerOn = true;
+    private GameObject navMesh;
 
-	public void OnTriggerStay(Collider other)
-	{
-		SetCollidingObject(other);
+    private SteamVR_Controller.Device Controller {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
 
-		if (Controller.GetAxis() != Vector2.zero)
-		{
-			if (Controller.GetAxis().y < 0f)
-			{
-				other.transform.Rotate(Vector3.up, rotationSpeed * Controller.GetAxis().y * 10 * Time.deltaTime);
-			}
-			if (Controller.GetAxis().y > 0f)
-			{
-				other.transform.Rotate(Vector3.down, rotationSpeed * -Controller.GetAxis().y * 10 * 2 * Time.deltaTime);
-			}            
-		}
 
-	}
+    private void Awake() {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
 
-	public void OnTriggerExit(Collider other)
-	{
-		if (!collidingObject)
-		{
-			return;
-		}
-		collidingObject = null;
-	}
+    private void Start() {
+        navMesh = GameObject.FindGameObjectWithTag("Navmesh");
+    }
 
-	private void GrabObject()
-	{
-		objectInHand = collidingObject;
-		collidingObject = null;
 
-		var joint = AddFixedJoint();
-		joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
-		objectInHandCheck = true;
-		if (objectInHand.GetComponent<HighlightController>())
-		{
-			objectInHand.GetComponent<HighlightController>().objectInHand = objectInHandCheck;
-		}
-	}
+    private void SetCollidingObject(Collider col) {
+        if (collidingObject || !col.GetComponent<Rigidbody>()) {
+            return;
+        }
+        collidingObject = col.gameObject;
+    }
 
-	private FixedJoint AddFixedJoint()
-	{
-		FixedJoint fx = gameObject.AddComponent<FixedJoint>();
-		fx.breakForce = 20000;
-		fx.breakTorque = 20000;
-		return fx;
-	}
+    public void OnTriggerEnter(Collider other) {
+        SetCollidingObject(other);
 
-	private void ReleaseObject()
-	{
-		if (GetComponent<FixedJoint>())
-		{
-			GetComponent<FixedJoint>().connectedBody = null;
-			Destroy(GetComponent<FixedJoint>());
+        myState = States.objectHighlighted;        
+    }
 
-			objectInHand.GetComponent<Rigidbody>().velocity = Controller.velocity;
-			objectInHand.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
-		}
-		objectInHandCheck = false;
-		if (objectInHand.GetComponent<HighlightController>())
-		{
-			objectInHand.GetComponent<HighlightController>().objectInHand = objectInHandCheck;
-		}
+    public void OnTriggerStay(Collider other) {
+        SetCollidingObject(other);
+    }
 
-		objectInHand = null;
-	}
+    public void OnTriggerExit(Collider other) {
+        if (!collidingObject) {
+            return;
+        }
+        collidingObject = null;
+        myState = States.freeRoam;
+    }
 
-	void Update () {
-		if (Controller.GetHairTriggerDown())
-		{
-			if (collidingObject)
-			{
-				GrabObject();
-			}
-		}
+    private void GrabObject() {
+        objectInHand = collidingObject;
+        collidingObject = null;
 
-		if (Controller.GetHairTriggerUp())
-		{
-			if (objectInHand)
-			{
-				ReleaseObject();
-			}
-		}		      
-	}
+        var joint = AddFixedJoint();
+        joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+        objectInHandCheck = true;
+        if (objectInHand.GetComponent<HighlightController>()) {
+            objectInHand.GetComponent<HighlightController>().objectInHand = objectInHandCheck;
+            myState = States.objectInHand;
+        }
+    }
+
+    private FixedJoint AddFixedJoint() {
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+
+    private void ReleaseObject() {
+        if (GetComponent<FixedJoint>()) {
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+
+            objectInHand.GetComponent<Rigidbody>().velocity = Controller.velocity;
+            objectInHand.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
+        }
+        objectInHandCheck = false;
+        if (objectInHand.GetComponent<HighlightController>()) {
+            objectInHand.GetComponent<HighlightController>().objectInHand = objectInHandCheck;
+        }
+
+        objectInHand = null;
+        myState = States.freeRoam;
+    }
+
+
+    void Update() {
+
+        if (Controller.GetHairTriggerDown()) {
+            if (collidingObject) {
+                GrabObject();
+            }
+        }
+
+        if (Controller.GetHairTriggerUp()) {
+            if (objectInHand) {
+                ReleaseObject();
+            }
+        }
+
+        if (myState == States.freeRoam) {
+            //I placed this event into an if statement because it was happening every frame
+            //which caused the pointer to constantly be enabled not allowing it to work correctly
+            //(teleporting through the floor).
+            if (!parabolicPointerOn) {
+
+                //This state setting here prevents all the popping around when the teleport should be off.
+
+
+                if (!menuOpen) {
+                    TurnOnParabolicPointer();
+                }
+            }
+
+
+        } else if (myState == States.objectHighlighted) {
+            if (parabolicPointerOn == true) {
+                TurnOffParabolicPointer();
+            }
+
+            if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad)) {
+                
+                if (!menuOpen) {
+
+                    GameObject menuInScene = GameObject.FindGameObjectWithTag("ContextMenuSystem");
+
+                    if (menuInScene == null) {
+                        GameObject contextMenu = (GameObject)Instantiate(contextMenuSystem);
+                        Vector3 offsetPos = new Vector3(0, 0.25f, 0.25f);
+                        Vector3 newPos = collidingObject.transform.position + offsetPos;
+                        contextMenu.transform.position = newPos;
+
+                        myState = States.menuActive;
+                        menuOpen = true;
+                    } 
+                }
+            }
+                       
+
+
+        } else if (myState == States.objectInHand) {
+            if (parabolicPointerOn == true) {
+                TurnOffParabolicPointer();
+            }
+
+
+        } else if (myState == States.menuActive) {
+            if (parabolicPointerOn == true) {
+                TurnOffParabolicPointer();  
+            }
+
+            //Ray raycast = new Ray(transform.position, transform.forward);
+            //RaycastHit rayHit;
+            //bool bHit = Physics.Raycast(raycast, out rayHit);
+            //if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad)) {
+            //    GameObject hitObject = rayHit.transform.gameObject;
+            //    hitObject.GetComponent<Button>().onClick.Invoke();
+            //}
+        }
+    }
+
+    private void TurnOffParabolicPointer() {
+        navMesh.SetActive(false);
+        parabolicPointer.SetActive(false);
+        parabolicPointer.GetComponent<ParabolicPointer>().enabled = false;
+        parabolicPointerOn = false;
+
+        //This state setting here prevents all the popping around when the teleport should be off.
+        TeleportVive teleportVive = FindObjectOfType<TeleportVive>();
+        teleportVive.CurrentTeleportState = TeleportState.None;
+    }
+
+    private void TurnOnParabolicPointer() {
+        navMesh.SetActive(true);       
+        parabolicPointer.SetActive(true);
+        parabolicPointer.GetComponent<ParabolicPointer>().enabled = true;
+
+        parabolicPointerOn = true; TeleportVive teleportVive = FindObjectOfType<TeleportVive>();
+        teleportVive.CurrentTeleportState = TeleportState.None;
+    }
 }
